c4acc4bbc0b1e8949cf0e4d6b18146c1e059fc24
muhammed <ajferaju9961@gmail.com>
2023-12-16 09:43:15 +0530

    Configured Private Routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,27 +13,36 @@ import BestSellingItems from "./pages/User/BestSellingItems";
 import SellerUsers from "./pages/Admin/Users";
 import SellerAddUser from "./pages/Admin/AddUser";
 import SellerEditUser from "./pages/Admin/EditUser";
+import { AuthProvider } from "./Utils/Auth";
+import PrivateRoutes from "./Utils/PrivateRoutes";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/category/bestSelling" element={<BestSellingItems />} />
+      <AuthProvider>
+        <Routes>
+          <PrivateRoutes>
+            <Route
+              path="/category/bestSelling"
+              element={<BestSellingItems />}
+            />
+          </PrivateRoutes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/home" element={<HomePage />} />
 
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/signup" element={<AdminSignup />} />
-        <Route path="/admin/home" element={<AdminLayout />} />
-        <Route path="/admin/products" element={<Products />} />
-        <Route path="/product/add" element={<AddProduct />} />
-        <Route path="/product/edit" element={<EditProduct />} />
-        <Route path="/admin/users" element={<SellerUsers />} />
-        <Route path="/admin/users/add" element={<SellerAddUser />} />
-        <Route path="/admin/users/edit" element={<SellerEditUser />} />
-      </Routes>
-      <Toaster position="top-center" reverseOrder={false} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/signup" element={<AdminSignup />} />
+          <Route path="/admin/home" element={<AdminLayout />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/product/add" element={<AddProduct />} />
+          <Route path="/product/edit" element={<EditProduct />} />
+          <Route path="/admin/users" element={<SellerUsers />} />
+          <Route path="/admin/users/add" element={<SellerAddUser />} />
+          <Route path="/admin/users/edit" element={<SellerEditUser />} />
+        </Routes>
+        <Toaster position="top-center" reverseOrder={false} />
+      </AuthProvider>
     </>
   );
 }
